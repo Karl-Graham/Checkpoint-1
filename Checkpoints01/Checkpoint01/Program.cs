d2d265fa281ea45561a729fe070527efb1dbb3c6
karlgraham <karlgraham@gmail.com>
2018-12-03 13:39:41 +0100

    Test 1

---
+++ Checkpoints01/Checkpoint01/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Checkpoint01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string input = GetUserInput();
+            Console.WriteLine();
+            string[] trianglesArray = CreateArrayofTriangles(input);
+            foreach (string i in trianglesArray)
+            {
+                DrawTriangle(i);                                              
+            }
+            Console.WriteLine();
+        }
+
+        private static string[] CreateArrayofTriangles(string input)
+        {
+            string[] trianglesArray = input.Split('-');
+            return trianglesArray;
+                                                       
+        }
+
+        private static string GetUserInput()
+        {
+            Console.Write("Write command: ");
+            string input = Console.ReadLine().ToLower();
+            return input;
+
+        }
+
+        private static void DrawTriangle(string l)
+        {
+                string m = l.Substring(1);
+                int t = int.Parse(m);
+
+            for (int i = 1; i <= t; i++)
+            {              
+                if (l[0] == 'a')
+                {
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+                if (l[0] == 'b')
+                {
+                    for (int j = 0; j <= t-i; j++)
+                    {
+                        Console.Write("*");
+                    }    
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
